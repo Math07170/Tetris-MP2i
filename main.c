bd2d1a282845d7fa005f650ef7aecf28f23f762c
Math07170 <mathiasr07@hotmail.fr>
2021-12-17 13:42:00 +0100

    C'est moche mais ça marche

--- main.c
+++ main.c
@@ -9,13 +9,23 @@
 
 int grille[20][10];
 
-gamestate state = {
-	0,
-	0,
-	0,
-	0,
-	1.0
-};
+gamestate state;
+
+void init_gamestate(){
+	state.block = 0;
+	state.rotation_index = 0;
+	state.x = 0;
+	state.y = 0;
+	state.game_speed = 5;
+	state.reserve_utilisee = false;
+	state.reserve = 0;
+	//Définir suivant
+	state.statut = 0;
+	state.descente_rapide = false;
+	state.compte_ligne = 0;
+	state.score = 0;
+	state.level = 0;
+}
 
 /* Agit sur le tetromino en cours de chute, selon l'entrée clavier */		// À déplacer...
 void interroge_commandes(gamestate* p_state, int grille[20][10]){
@@ -32,19 +42,18 @@ int play = 0;		// 0 : Jeu en cours ; 1 Pause ; 2 Perdu
 void tick(){
 	play = nouveau_tetromino(&state, grille);
 	int tick_count = 0;
-	int delai_descentes = 7;
 	bool avance_rapide = false;
 	int lignes_supprimees = 0;
     while(play == 0){
 		interroge_commandes(&state,grille);
 		affiche_grille(grille);
-		if(tick_count == delai_descentes){
+		if(tick_count == state.game_speed){
 			tick_count = 0;
 			if(descente_possible(&state,grille) == true){
 				descend(&state, grille);
 			}else{
 				fixe_tetromino(state, grille);
-				lignes_supprimees += nettoie_lignes(grille);
+				state.compte_ligne += nettoie_lignes(grille);
 				play = nouveau_tetromino(&state, grille);
 			}
 		}
@@ -54,6 +63,7 @@ void tick(){
 }
 
 int main() {
+	init_gamestate();
 	init_tetrominos();
     init_ncurses();
     initialise_grille(grille);
--- utils.h
+++ utils.h
@@ -2,13 +2,22 @@
 #define UTILS_H
 
 #include <time.h>
+#include <stdbool.h>
 
 typedef struct {
     int block;
     int rotation_index;
     int x;
     int y;
-    float game_speed; // à virer
+    int game_speed;
+    bool reserve_utilisee;
+    int reserve;
+    int suivants[4];
+    int statut;
+    bool descente_rapide;
+    int compte_ligne;
+    int score;
+    int level;
 } gamestate;
 
 void wait(double time);
